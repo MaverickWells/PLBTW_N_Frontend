43bf4b9ba9be645d664f5ce2dfc9e87c94d81816
Welly Wells <wellyngg@gmail.com>
2016-11-17 22:03:05 +0700

    add font, js, css, img needed add news model add news controller add news view page first setting etc

---
+++ application/models/news_model.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class NEWS_MODEL extends CI_Model {
+	// public function CreateNews($data)
+	// {
+	// 	$this->db->insert('news', $data);
+	//
+	// 	return $this->db->affected_rows();
+	// }
+
+	// public function GetLocationNews($location)
+	// {
+	// 	$query = $this->db->get_where('news', array('location' => $location));
+	// 	//var_dump($query->row_array());
+	// 	return $query->row_array();
+	// }
+
+	public function GetAllNews()
+	{
+		$query = $this->db->get('news');
+
+		return $query->result();
+	}
+
+	// public function GetNews($id)
+	// {
+	// 	$query = $this->db->get_where('news', array('id_news' => $id));
+	// 	//var_dump($query->row_array());
+	// 	return $query->row_array();
+	// }
+
+	// public function DeleteNews($id)
+	// {
+	// 	$this->db->where('id_news', $id);
+    //     $this->db->delete('news');
+	// }
+	//
+	// public function UpdateNews($data, $idnews)
+	// {
+	// 	$this->db->where('id_news', $idnews);
+	// 	$this->db->update('news', $data);
+	// }
+}
